39e383811415ed6c4ae742e691143246ec9abef6
fiqinovrian <fiqinovrian@gmail.com>
2022-05-30 21:49:16 +0700

    Update Console Log Index

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import fungsiLuasPersegiPanjang from "./luasPersegiPanjang.js";
 import fungsiLuasLingkaran from "./luas_lingkaran.js";
 
+console.log("Ini Fungsi Luas Persegi Panjang");
 console.log(fungsiLuasPersegiPanjang(2, 2));
+console.log("Ini fungsi luas lingkaran");
 console.log(fungsiLuasLingkaran(7));
