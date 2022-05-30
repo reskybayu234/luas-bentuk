2db932cc526515a25f628089c269d83fbf08fa8c
reskybayu234 <75589544+reskybayu234@users.noreply.github.com>
2022-05-30 23:03:46 +0700

    function di dalam class

---
+++ index2.js
@@ -0,0 +1,11 @@
+class Perhitungan {
+  luasJajarGenjang(alas, tinggi) {
+    return alas * tinggi;
+  }
+  luasLingkaran(r) {
+    const phi = 3.14;
+    return (phi * r) ** 2;
+  }
+}
+
+export { Perhitungan };
